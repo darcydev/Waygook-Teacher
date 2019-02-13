685848fedf1fd55eb2a7a98b236d21b2ff32fda1
Darcy Price <darcyelsing@gmail.com>
2019-02-13 10:14:13 +0900

    included seperate index file for T/S

---
+++ index-student.php
@@ -0,0 +1,42 @@
+<?php
+include("includes/header.php")
+?>
+
+<div id="main-content-container">
+    <div id="teacher-index-container" class="index-container">
+        <div class="index-heading">
+            <h2>Welcome to Waygook Teacher!</h2>
+            <h4>platform to connect Korean students with English teachers</h4>
+        </div>
+        <div class="index-steps">
+            <div id="teacher-index-text-step-one" class="index-text-step-one">
+                <p>We have many experienced English teachers, search for one you like!</p>
+                <img src="" alt="search-teacher-img">
+            </div>
+            <div id="teacher-index-text-step-two" class="index-text-step-two">
+                <p>Send a message to arrange a lesson at your convience!</p>
+                <img src="" alt="send-message-img">
+            </div>
+            <div id="teacher-index-text-step-three" class="index-text-step-three">
+                <p>Private lesson on Skype tailored to your levels and goals!</p>
+                <img src="" alt="skype-lesson-img">
+            </div>
+        </div>
+        <div class="index-welcome">
+            <p>
+                Learn from experienced English teachers, from all over the world!
+                <br>Practice your English through Skype, at a time of your convenience
+                to fit within your schedule!
+                <br>Private lessons specifically tailored for your English
+                levels and suited to achieve your personal English goals!
+                <br>Prepare for interview, practice prounounication and idioms
+                with a native speaker, study grammer for a test, improve conversation
+                practice to travel or study abroad!
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+include("includes/footer.php")
+?>
---
+++ index-teacher.php
@@ -0,0 +1,37 @@
+<?php
+include("includes/header.php")
+?>
+
+<div id="main-content-container">
+    <div id="teacher-index-container" class="index-container">
+        <div class="index-heading">
+            <h2>Welcome to Waygook Teacher!</h2>
+            <h4>platform to connect Korean students with English teachers</h4>
+        </div>
+        <div class="index-steps">
+            <div id="teacher-index-text-step-one" class="index-text-step-one">
+                <p>Complete your profile, with recent photo and description of your teaching experience</p>
+                <img src="" alt="complete-profile-img">
+            </div>
+            <div id="teacher-index-text-step-two" class="index-text-step-two">
+                <p>Students will search for you, and send you a message</p>
+                <img src="" alt="search-teacher-img">
+            </div>
+            <div id="teacher-index-text-step-three" class="index-text-step-three">
+                <p>Arrange a lesson on Skype with Students through message </p>
+                <img src="" alt="skype-lesson-img">
+            </div>
+        </div>
+        <div class="index-welcome">
+            <p>
+                Teach Korean students, of all ages and levels in improving their English!
+                <br>Earn income working from home at your convience!
+                <br>Private lessons specifically tailored to your strengths and teaching style
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+include("includes/footer.php")
+?>
