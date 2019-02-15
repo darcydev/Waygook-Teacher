e5dae5d87c0c722c32956a8bc06a435fe8a0266d
Darcy Price <darcyelsing@gmail.com>
2019-02-15 15:47:25 +0900

    REFACTOR: cleaned up edit-profile.php

--- edit-profile.php
+++ edit-profile.php
@@ -4,6 +4,26 @@ include("includes/header.php")
 
 <div id="edit-profile-container">
     <div id="edit-profile-content">
+        <form id="edit-profile-form" class="form" method="post" enctype="multipart/form-data">
+            <p>
+                <?php echo $user->getError(Constants::$invalidFileExtension); ?>
+                <?php echo $user->getError(Constants::$invalidFileSize); ?>
+                <?php echo $user->getError(Constants::$invalidFileName); ?>
+                <label for="upload-profile-pic">Upload Profile Picture</label>
+                <input id="upload-profile-pic" type="file" name="upload-profile-pic">
+            </p>
+            <p>
+                <?php echo $user->getError(Constants::$descriptionCharacters); ?>
+                <label for="edit-description">Profile Description</label>
+                <textarea id="edit-description" type="text" name="edit-description"></textarea>
+            </p>
+            <button type="submit" name="edit-profile-button">CONFRIM</button>
+        </form>
+
+
+
+
+        <!--
         <form id="edit-pic-form" class="edit-profile-form" method="post" enctype="multipart/form-data">
             <?php echo $user->getError(Constants::$invalidFileExtension); ?>
             <?php echo $user->getError(Constants::$invalidFileSize); ?>
@@ -18,6 +38,7 @@ include("includes/header.php")
             <textarea id="edit-description" type="text" name="edit-description"></textarea>
             <button type="submit" name="edit-profile-button">Confirm Description Changes</button>
         </form>
+        -->
     </div>
 </div>
 
--- includes/handlers/edit-profile-handler.php
+++ includes/handlers/edit-profile-handler.php
@@ -1,42 +1,47 @@
 <?php
-if (isset($_POST['confirm-profile-pic-button']) && isset($_FILES["upload-profile-pic"])) {
-    $currentDir = getcwd();
-    $targetDir = "assets/images/profile-pics/";
-    // save the file as a random number ahead of fileName (fe "283572893572875johnlennon.png")
-    // to (largely) avoid issue of duplicate file names in server
-    // BUG: rand() is not secure (is predictable), consider using random_bytes()
-    $randomNumber = rand();
-    // add $randomNumber to beginning of fileName
-    $fileName = $randomNumber . $_FILES["upload-profile-pic"]["name"];
-    $fileSize = $_FILES["upload-profile-pic"]["size"];
-    $fileTmpName = $_FILES["upload-profile-pic"]["tmp_name"];
-    $fileExtension = strtolower(end(explode('.', $fileName)));
-    $uploadPath = $currentDir . "/" . $targetDir . basename($fileName);
-    // POTENTIAL BUG: may have to remove "/" in "/assets"
-    $db_uploadPath = $targetDir . basename($fileName);
+if (isset($_POST['edit-profile-button'])) {
 
-    $rowsAffected = $user->updateProfilePic($db_uploadPath, $fileExtension, $fileSize, $uploadPath);
+    // If the User has uploaded a profile pic in the form
+    if (isset($_FILES['upload-profile-pic'])) {
+        $currentDir = getcwd();
+        $targetDir = "assets/images/profile-pics/";
+        // save the file as a random number ahead of fileName (fe "283572893572875johnlennon.png")
+        // to (largely) avoid issue of duplicate file names in server
+        // BUG: rand() is not secure (is predictable), consider using random_bytes()
+        $randomNumber = rand();
+        // add $randomNumber to beginning of fileName
+        $fileName = $randomNumber . $_FILES["upload-profile-pic"]["name"];
+        $fileSize = $_FILES["upload-profile-pic"]["size"];
+        $fileTmpName = $_FILES["upload-profile-pic"]["tmp_name"];
+        $fileExtension = strtolower(end(explode('.', $fileName)));
+        $uploadPath = $currentDir . "/" . $targetDir . basename($fileName);
+        // POTENTIAL BUG: may have to remove "/" in "/assets"
+        $db_uploadPath = $targetDir . basename($fileName);
 
-    if($rowsAffected > 0) {
-        $successUpload = move_uploaded_file($fileTmpName, $uploadPath);
-        if ($successUpload) {
-            echo "Picture successfully updated";
-        } else {
-            echo "Sorry, there has been an error uploading the photo";
+        $rowsAffected = $user->updateProfilePic($db_uploadPath, $fileExtension, $fileSize, $uploadPath);
+
+        if($rowsAffected > 0) {
+            $successUpload = move_uploaded_file($fileTmpName, $uploadPath);
+            if ($successUpload) {
+                echo "Picture successfully updated";
+            } else {
+                echo "Sorry, there has been an error uploading the photo";
+            }
         }
     }
-}
 
-if(isset($_POST['edit-profile-button']) && isset($_POST['edit-description'])) {
-    // BUG: sanitize this input (???)
-    $desc = $_POST['edit-description'];
+    // If the User has completed the description in the form
+    if (isset($_POST['edit-description'])) {
+        // BUG: sanitize this input (???)
+        $desc = $_POST['edit-description'];
 
-    $rowsAffected = $user->updateDescription($desc);
+        $rowsAffected = $user->updateDescription($desc);
 
-    if($rowsAffected > 0) {
-        echo "Description successfully updated";
-    } else {
-        echo "Sorry, there has been an error updating your profile.";
+        if($rowsAffected > 0) {
+            echo "Description successfully updated";
+        } else {
+            echo "Sorry, there has been an error updating your profile.";
+        }
     }
 }
 ?>
