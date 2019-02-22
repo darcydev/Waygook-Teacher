41678165f1393af4ecdd9a6a8b36e7ffcbb13885
Darcy Price <darcyelsing@gmail.com>
2019-02-22 13:22:48 +0900

    WIP: styling settings.php

--- settings.php
+++ settings.php
@@ -14,7 +14,21 @@ include("includes/header.php");
                 <div class="box-head-title">Change Password</div>
             </div>
             <div class="box-content">
-
+                <form class="form" method="post">
+                    <p>
+                        <label for="old-password">Current password</label>
+                        <input id="old-password" name="old-password" type="password" required>
+                    </p>
+                    <p>
+                        <label for="new-password">New password</label>
+                        <input id="new-password" name="new-password" type="password" required>
+                    </p>
+                    <p>
+                        <label for="new-password2">New password</label>
+                        <input id="new-password2" name="new-password2" type="password" required>
+                    </p>
+                    <button class="button" type="submit" name="login-button">SAVE PASSWORD</button>
+                </form>
             </div>
         </div>
     </div>
