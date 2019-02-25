cdd05c296f5122ba91b657be04d47e6f8498b2e8
darcyprice <darcyelsing@gmail.com>
2019-02-25 21:35:32 +0900

    split register and home.php into seperate files

---
+++ home.php
@@ -0,0 +1,191 @@
+<html>
+<head>
+    <title>Waygook Teacher</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="landing-page-container">
+        <div class="landing-page-row">
+            <div class="welcome-text box">
+                <h1>Learn English, right now</h1>
+                <h2>Connect with and learn from native English speakers</h2>
+                <ul>
+                    <li>Study vocabulary and grammar for exams</li>
+                    <li>Improve pronounciation with native speaker</li>
+                    <li>Prepare for interviews</li>
+                </ul>
+            </div>
+            <div class="box">
+                <div class="box-icon">
+                    <img src="" alt="box-icon">
+                </div>
+                <div class="box-title">
+                    <h2>Make the World accessible to you</h2>
+                    <h3>Speak naturally with professional English teachers
+                    from around the world</h3>
+                </div>
+                <div class="box-img">
+                    <img src="" alt="teacher-globe-image">
+                </div>
+                <div class="box-content">
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="assets/images/icons/icons8-diploma_2.png" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Expert Tutors</div>
+                                <div class="post-content">Find native speakers and certified private tutors</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="assets/images/icons/icons8-diploma_2.png" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Verified profiles</div>
+                                <div class="post-content">We carefully check and confirm each tutor's profile</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Learn anytime</div>
+                                <div class="post-content">Take video lessons anytime that suits you. Tutors available 24/7.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Affordable prices</div>
+                                <div class="post-content">Choose an experienced tutor that fits your budget.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="box-icon">
+                    <img src="" alt="box-icon">
+                </div>
+                <div class="box-title">
+                    <h2>Focus on the skills you need</h2>
+                    <h3>Prepare to achieve your goals with private tutors</h3>
+                </div>
+                <div class="box-content">
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Immerse yourself in a new culture</div>
+                                <div class="post-content">Connect with language experts from around the world.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Succeed in your career</div>
+                                <div class="post-content">Develop your working vocabulary and communicate clearly.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Get expert help when you need it</div>
+                                <div class="post-content">Learn to solve any problem in any language.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Speak naturally, always</div>
+                                <div class="post-content">Make a good impression and build trust in anylanguage.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="box-icon">
+                    <img src="" alt="box-icon">
+                </div>
+                <div class="box-title">
+                    <h2>How WaygookTeacher works</h2>
+                    <h3>Learn languages online with the world's best tutors</h3>
+                </div>
+                <div class="box-content">
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Speak naturally, always</div>
+                                <div class="post-content">Make a good impression and build trust in anylanguage.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Speak naturally, always</div>
+                                <div class="post-content">Make a good impression and build trust in anylanguage.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Speak naturally, always</div>
+                                <div class="post-content">Make a good impression and build trust in anylanguage.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-content-row">
+                        <div class="post">
+                            <div class="post-icon">
+                                <img src="" alt="post-icon">
+                            </div>
+                            <div class="post-main">
+                                <div class="post-title">Speak naturally, always</div>
+                                <div class="post-content">Make a good impression and build trust in anylanguage.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- register.php
+++ register.php
@@ -37,125 +37,114 @@ function roleCheck(that) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-    <div id="background">
-        <div id="login-container">
-            <div id="input-container">
-                <form id="login-form" class="register-form" action="register.php" method="post">
-                    <h2>Login</h2>
+    <div class="register-container">
+        <div class="register-forms">
+            <div class="register-form form">
+                <h2>Login</h2>
+                <p>
+                    <?php echo $account->getError(Constants::$loginFailed); ?>
+                    <label for="login-username">Username</label>
+                    <input id="login-username" name="login-username" type="text" required>
+                </p>
+                <p>
+                    <label for="login-password">Password</label>
+                    <input id="login-password" name="login-password" type="password" required>
+                </p>
+                <button type="submit" name="login-button" class="button">LOG IN</button>
+            </div>
+            <div class="register-form form">
+                <h2>Register</h2>
+                <p>
+                    <label for="role">Role</label>
+                    <select id="role" name="role" type="text" class="select" onchange="roleCheck(this);" required>
+                        <option value="teacher">Teacher</option>
+                        <option value="student">Student</option>
+                    </select>
+                </p>
+                <p>
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
+                    <label for="username">Username</label>
+                    <input id="username" name="username" type="text" value="<?php getInputValue('username') ?>" required>
+                </p>
+                <p>
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                    <label for="first-name">First name</label>
+                    <input id="first-name" name="first-name" type="text" value="<?php getInputValue('firstName') ?>" required>
+                </p>
+                <p>
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                    <label for="last-name">Last name</label>
+                    <input id="last-name" name="last-name" type="text" value="<?php getInputValue('lastName') ?>" required>
+                </p>
+                <p>
+                    <?php echo $account->getError(Constants::$emailInvalid); ?>
+                    <?php echo $account->getError(Constants::$emailTaken); ?>
+                    <label for="email">Email</label>
+                    <input id="email" name="email" type="email" value="<?php getInputValue('email') ?>" required>
+                </p>
+                <p>
+                    <?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+                    <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                    <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                    <label for="password">Password</label>
+                    <input id="password" name="password" type="password" required>
+                </p>
+                <p>
+                    <label for="password2">Confirm password</label>
+                    <input id="password2" name="password2" type="password" required>
+                </p>
+                <div id="teacher-register-form" class="register-form">
                     <p>
-                        <?php echo $account->getError(Constants::$loginFailed); ?>
-                        <label for="login-username">Username</label>
-                        <input id="login-username" name="login-username" type="text" required>
-                    </p>
-                    <p>
-                        <label for="login-password">Password</label>
-                        <input id="login-password" name="login-password" type="password" required>
-                    </p>
-                    <button type="submit" name="login-button">LOG IN</button>
-                </form>
-                <form id="register-form" class="register-form" action="register.php" method="post">
-                    <h2>Register</h2>
-                    <p>
-                        <label for="role">Role</label>
-                        <select id="role" name="role" type="text" onchange="roleCheck(this);" required>
-                            <option value="teacher">Teacher</option>
-                            <option value="student">Student</option>
+                        <label for="gender">Gender</label>
+                        <select id="gender" name="gender" class="select" type="text">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
                         </select>
                     </p>
                     <p>
-                        <?php echo $account->getError(Constants::$usernameCharacters); ?>
-                        <?php echo $account->getError(Constants::$usernameTaken); ?>
-                        <label for="username">Username</label>
-                        <input id="username" name="username" type="text" value="<?php getInputValue('username') ?>" required>
+                        <label for="nationality">Nationality</label>
+                        <select id="nationality" name="nationality" class="select" type="text">
+                            <option value="american">American</option>
+                            <option value="australian">Australian</option>
+                            <option value="british">British</option>
+                            <option value="canadian">Canadian</option>
+                            <option value="southAfrica">South African</option>
+                            <option value="filipino">Filipino</option>
+                        </select>
                     </p>
                     <p>
-        				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
-                        <label for="first-name">First name</label>
-                        <input id="first-name" name="first-name" type="text" value="<?php getInputValue('firstName') ?>" required>
+                        <label for="educationLevel">Education Level</label>
+                        <select id="educationLevel" name="educationLevel" class="select"  type="text">
+                            <option value="teritary">Teritary</option>
+                            <option value="diploma">Diploma</option>
+                            <option value="bachelor">Bachelor</option>
+                            <option value="masters">Masters</option>
+                            <option value="doctorate">Doctorate</option>
+                        </select>
                     </p>
                     <p>
-                        <?php echo $account->getError(Constants::$lastNameCharacters); ?>
-                        <label for="last-name">Last name</label>
-                        <input id="last-name" name="last-name" type="text" value="<?php getInputValue('lastName') ?>" required>
+                        <label for="educationMajor">Education Major</label>
+                        <select id="educationMajor" name="educationMajor" class="select"  type="text">
+                            <option value="none">None</option>
+                            <option value="education">Education</option>
+                            <option value="english">English</option>
+                            <option value="business">Business</option>
+                            <option value="communication">Communication</option>
+                            <option value="engineering">Engineering</option>
+                            <option value="maths">Maths</option>
+                            <option value="law">Law</option>
+                            <option value="social_sciences">Social Sciences</option>
+                            <option value="arts">Arts</option>
+                            <option value="other">Other</option>
+                        </select>
                     </p>
                     <p>
-                        <?php echo $account->getError(Constants::$emailInvalid); ?>
-                        <?php echo $account->getError(Constants::$emailTaken); ?>
-                        <label for="email">Email</label>
-                        <input id="email" name="email" type="email" value="<?php getInputValue('email') ?>" required>
+                        <label for="dob">Date of Birth</label>
+                        <input id="dob" name="dob" type="date">
                     </p>
-                    <p>
-                        <?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
-                        <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                        <?php echo $account->getError(Constants::$passwordCharacters); ?>
-                        <label for="password">Password</label>
-                        <input id="password" name="password" type="password" required>
-                    </p>
-                    <p>
-                        <label for="password2">Confirm password</label>
-                        <input id="password2" name="password2" type="password" required>
-                    </p>
-                    <div id="teacher-register-form" class="register-form">
-                        <p>
-                            <label for="gender">Gender</label>
-                            <select id="gender" name="gender" type="text">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                        </p>
-                        <p>
-                            <label for="nationality">Nationality</label>
-                            <select id="nationality" name="nationality" type="text">
-                                <option value="american">American</option>
-                                <option value="australian">Australian</option>
-                                <option value="british">British</option>
-                                <option value="canadian">Canadian</option>
-                                <option value="southAfrica">South African</option>
-                                <option value="filipino">Filipino</option>
-                            </select>
-                        </p>
-                        <p>
-                            <label for="educationLevel">Education Level</label>
-                            <select id="educationLevel" name="educationLevel" type="text">
-                                <option value="teritary">Teritary</option>
-                                <option value="diploma">Diploma</option>
-                                <option value="bachelor">Bachelor</option>
-                                <option value="masters">Masters</option>
-                                <option value="doctorate">Doctorate</option>
-                            </select>
-                        </p>
-                        <p>
-                            <label for="educationMajor">Education Major</label>
-                            <select id="educationMajor" name="educationMajor" type="text">
-                                <option value="none">None</option>
-                                <option value="education">Education</option>
-                                <option value="english">English</option>
-                                <option value="business">Business</option>
-                                <option value="communication">Communication</option>
-                                <option value="engineering">Engineering</option>
-                                <option value="maths">Maths</option>
-                                <option value="law">Law</option>
-                                <option value="social_sciences">Social Sciences</option>
-                                <option value="arts">Arts</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </p>
-                        <p>
-                            <label for="dob">Date of Birth</label>
-                            <input id="dob" name="dob" type="date">
-                        </p>
-                    </div>
-                    <button type="submit" name="register-button">SIGN UP</button>
-                </form>
-            </div>
-            <div id="login-text">
-                <h1>Study English, right now</h1>
-                <h2>Connect with and learn from native English speakers</h2>
-                <ul>
-                    <li>Study vocabulary and grammar for exams</li>
-                    <li>Improve pronounciation with native speaker</li>
-                    <li>Prepare for interviews</li>
-                </ul>
+                </div>
+                <button type="submit" name="register-button" class="button">SIGN UP</button>
             </div>
         </div>
     </div>
