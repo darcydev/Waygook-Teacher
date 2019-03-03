6c8d76514a28013958a7f03a11de492f41a15b09
darcyprice <darcyelsing@gmail.com>
2019-03-03 11:01:27 +0900

    FUNC: implemented HTML and CSS of schedule lesson container

--- includes/schedule-lesson-form.php
+++ includes/schedule-lesson-form.php
@@ -2,7 +2,7 @@
     <div class="overlay-container">
         <div class="schedule-form auth-form form">
             <h3>Schedule lesson</h3>
-            <div id="schedule-lesson-close" class="menu-button">
+            <div id="schedule-lesson-close" class="close-button menu-button">
                 <img src="assets/images/icons/icons8-globe.png" alt="close-button">
             </div>
             <p>
@@ -19,9 +19,6 @@
             </p>
             <p>
                 <label for="lesson-student">Select student</label>
-                <div id="choose-student-open" class="menu-button">
-                    <img src="assets/images/icons/icons8-globe.png" alt="close-button">
-                </div>
                 <div id="choose-student-list">
                     <ul id='choose-student-list' class='choose-list user-list list'>
                     <?php
@@ -40,18 +37,24 @@
                             $otherUserID = $employment_row['teacher_id'];
                         }
 
+                        // set var prepaid_hours for each employment
+                        $prepaid_amount = $employment_row['prepaid_amount'];
+                        $rate = $employment_row['rate'];
+                        $prepaid_hours = $prepaid_amount / $rate;
+
                         // fetch the User details for the 'other user'
                         $sql = "SELECT * FROM Users WHERE userID = ?";
                         $stmt = $db->run($sql, [$otherUserID]);
                         $other_user_row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                         foreach ($other_user_row as $user_row) {
+
                             echo "<li id='schedule_user_" . $otherUserID . "' class='choose-student'>
                                     <div class='info-content profile-photo-small'>
                                         <img src=" . $user_row['profile_pic'] . " alt='profile-pic'>
                                     </div>
                                     <p>" . $user_row['first_name'] . "</p>
-                                    <p>" . $employment_row['prepaid_amount'] . "</p>
+                                    <span class='gray'>" . $prepaid_hours . " hrs</span>
                                 </li>";
                         }
                     }
